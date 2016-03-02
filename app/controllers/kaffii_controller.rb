364987c56acc6d4cf0940bed0dc69b0f5bfda8de
danieladler <weston.adler@gmail.com>
2016-03-01 16:39:29 -0800

    views, routes and specs for 'create'

--- app/controllers/kaffii_controller.rb
+++ app/controllers/kaffii_controller.rb
@@ -1,6 +1,27 @@
 class KaffiiController < ApplicationController
 
   def index
+    @kaffiis = Kaffii.all
+  end
+
+  def show
+    @kaffii = Kaffii.find(params[:id])
+  end
+
+  def create
+    @kaffii = Kaffii.new
+  end
+
+  def add
+    @kaffii = Kaffii.new
+    @kaffii.name = params[:name]
+    @kaffii.origin = params[:origin]
+    @kaffii.roast = params[:roast]
+    if @kaffii.save
+      redirect_to root_path
+    else
+      render :create
+    end
   end
 
 end
--- spec/controllers/kaffii_controller_spec.rb
+++ spec/controllers/kaffii_controller_spec.rb
@@ -13,36 +13,54 @@ RSpec.describe KaffiiController, type: :controller do
     # end
   end
 
-
-  
-
   describe "GET show" do
+    let(:kaffii) {Kaffii.create(name:"test", origin: "Oregon", roast:"bland")}
+
     it "returns 200" do
-      get :show
+      get :show, id: kaffii.id
       expect(response.status).to eq 200
     end
 
-    it "has a single kaffii" do
-      # expect id in params to be same id as kaffii on the view
+    it "matches the right instance" do
+      get :show, id: kaffii.id
+      expect(assigns(:kaffii).id).to eq(kaffii.id)
     end
   end
 
-
-
-
-  describe "POST create" do
-    it "adds a kaffii to the database" do
-      count = Kaffii.count
-      post :create, kaffii: {name: "test", origin: "Iceland", roast: "Very Dark"} # 2nd arg here is params
-      expect(Kaffii.count).to eq count + 1
+  describe "GET create-post form" do
+    it "returns 200" do
+      get :create
+      expect(response.status).to eq 200
     end
 
-   # it "redirects to home page" do
-   #   post :create, kaffii: {name: "second", origin: "Brazil", roast: "Medium"} # 2nd arg here is params
-   #   expect(response).to redirect_to "/"
-   #   # ALT: expect(response.location).to eq "/"
-   # end
+    it "starts with a new, unsaved instance" do
+      get :create
+      expect(assigns(:kaffii).id).to eq nil
+    end
   end
+  #
+  # describe "POST create" do
+  #   it "adds a kaffii to the database" do
+  #     count = Kaffii.count
+  #     post :create, kaffii: {name: "Test", origin: "Iceland", roast: "Very Dark"} # 2nd arg here is params
+  #     expect(Kaffii.count).to eq count + 1
+  #   end
+  #
+  #  # it "redirects to home page" do
+  #  #   post :create, kaffii: {name: "second", origin: "Brazil", roast: "Medium"} # 2nd arg here is params
+  #  #   expect(response).to redirect_to "/"
+  #  #   # ALT: expect(response.location).to eq "/"
+  #  # end
+  # end
+
+  # describe "DELETE destroy" do
+    # pass in specific kaffii from params
+
+    # it "removes a kaffii from the database" do
+    #   count = Kaffii.count
+    #   expect {delete :destroy, id: [current_kaffii.id]}.to change { Kaffii.count}.by(-1)
+    # end
+  # end
 
 
 
